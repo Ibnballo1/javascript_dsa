f277c9dd9cd811439ac325c3ae9d2aa8bfbcc3e2
Ibnballo1 <belloabdullateef035@gmail.com>
2023-12-04 07:18:17 +0100

    Solve minmax

--- minimaxsum/minmaxSum.js
+++ minimaxsum/minmaxSum.js
@@ -4,44 +4,24 @@
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-// function miniMaxSum(arr) {
-//     // Write your code here
-//     let minSum = Infinity, maxSum = 0;
-//     for (let i = 0; i < arr.length; i++) {
-//         let sum = 0
-//         for (let j = 0; j < arr.length; j++) {
-//             if (i !== j) {
-//                 sum += arr[j]
-//             }
-//         }
-//         // (sum > maxSum) ? maxSum = sum : minSum = sum
-//         if (sum > maxSum) {
-//             maxSum = sum
-//         }
-//         if (sum < minSum) {
-//             minSum = sum
-//         }
-//     }
-//     return `${minSum} ${maxSum}`
-// }
-
 function miniMaxSum(arr) {
     // Write your code here
-    let runningSum = 0;
+    let minSum = 0, maxSum = 0, sumArr = [];
     for (let i = 0; i < arr.length; i++) {
-        runningSum += arr[i];
+        let sum = 0
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j) {
+                sum += arr[j]
+            }
+        }
+        sumArr.push(sum);
     }
 
-    let minSum = runningSum, maxSum = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        let currentSum = runningSum - arr[i];
-        (currentSum > maxSum) ? maxSum = currentSum : (currentSum < minSum) ? minSum = currentSum : null;
-    }
-
-    return `${minSum} ${maxSum}`;
+    minSum = Math.min(...sumArr);
+    maxSum = Math.max(...sumArr);
+    return `${minSum} ${maxSum}`
 }
 
-console.log(miniMaxSum([1, 3, 5, 7, 9]));
-console.log(miniMaxSum([1, 2, 3, 4, 5]));
-console.log(miniMaxSum([140638725, 43625791, 953274816, 734065819, 362748590])); // 1673711044 2486347135
+console.log(miniMaxSum([1, 3, 5, 7, 9])); // 16 24
+console.log(miniMaxSum([1, 2, 3, 4, 5])); // 10 14
+console.log(miniMaxSum([140638725, 43625791, 953274816, 734065819, 362748590])); // 1281078925 2190727950
